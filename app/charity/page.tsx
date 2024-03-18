bf0cccfa5a1ed26b4bc4ef49caeb50a9f7a5b33b
evilwizard13 <evilwizard13@protonmail.com>
2024-03-18 15:39:11 +0000

    linked to values

--- app/charity/page.tsx
+++ app/charity/page.tsx
@@ -7,30 +7,31 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
 import Icon from "@mdi/react";
-import { mdiAccount, mdiEmail, mdiMenu, mdiPhone } from '@mdi/js';
+import { mdiAccount, mdiEmail, mdiMapMarker, mdiMenu, mdiPhone } from '@mdi/js';
 import { useEffect } from 'react';
 import { AppBar, Box, IconButton, Pagination, Toolbar } from '@mui/material';
 import { SideDrawer } from '@/components/components';
+import values from "../../values.json"
 
 export default function Home() {
 
-    const list = [0, 0, 0, 0]//, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]//, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     return (
 
         <div className='w-full'>
             <AppBar position="sticky">
                 <Toolbar sx={{ width: "100%", maxWidth: 600, mx: "auto" }}>
-                    <SideDrawer/>
+                    <SideDrawer />
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Title
                     </Typography>
 
                 </Toolbar>
             </AppBar>
-            <div className='flex flex-col gap-4 m-4'>
+            <div className='flex grid-cols-1 lg:grid-cols-3 grid gap-4 m-4'>
                 {list.map((value, index) => (
-                    <Item key={index} index={index} />
+                    <Item key={index} data={values.items[index % 6]} index={index} />
                 ))}
             </div>
             <Pagination count={11} defaultPage={1} className="my-4 w-fit mx-auto" />
@@ -39,38 +40,31 @@ export default function Home() {
     )
 }
 
-
-
-function Item({ index }: { index: number }) {
-
-
+function Item({ index, data }: { index: number, data: any }) {
 
     return (
         <>
-
             <Card >
                 <CardMedia
                     image={`/${index % 6 + 1}.jpg`}
                     sx={{ height: 200 }}
                     title="skip"
-                ></CardMedia>
-
-
+                />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                        12 random items
+                        {`(${data.quantity}) ${data.item}`}
                     </Typography>
                     <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
                         <Icon path={mdiAccount} size={1.0} />
-                        Building company name
+                        {data.company}
                     </Typography>
                     <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
-                        <Icon path={mdiEmail} size={1.0} />
-                        Name@Website.com
+                        <Icon path={mdiMapMarker} size={1.0} />
+                        {data.location}
                     </Typography>
                     <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
                         <Icon path={mdiPhone} size={1.0} />
-                        07534 123445
+
                     </Typography>
                 </CardContent>
                 <CardActions>
