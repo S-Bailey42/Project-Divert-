cada39da3fcfc3919cc6e884771f6ab65bcc9120
evilwizard13 <evilwizard13@protonmail.com>
2024-04-15 15:59:13 +0100

    add more stuff

--- app/charity/page.tsx
+++ app/charity/page.tsx
@@ -9,125 +9,128 @@ import Image from 'next/image';
 import Icon from "@mdi/react";
 import { mdiAccount, mdiChevronDown, mdiChevronUp, mdiEmail, mdiMagnify, mdiMapMarker, mdiMenu, mdiPhone, mdiTune } from '@mdi/js';
 import { useEffect } from 'react';
-import { AppBar, Box, Divider, Drawer, IconButton, InputBase, Pagination, Paper, Stack, TextField, Toolbar } from '@mui/material';
+import { AppBar, Box, Divider, Drawer, IconButton, InputBase, Pagination, Paper, Slider, Stack, TextField, Toolbar } from '@mui/material';
 import { SideDrawer } from '@/components/components';
 import { useState } from 'react';
 import values from "../../values.json"
 
 const TopBar = () => {
-    return (
-        <AppBar position="sticky">
-            <Toolbar sx={{ width: "100%", maxWidth: 600, mx: "auto" }}>
-                <SideDrawer />
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Title
-                </Typography>
+  return (
+    <AppBar position="sticky">
+      <Toolbar sx={{ width: "100%", maxWidth: 600, mx: "auto" }}>
+        <SideDrawer />
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Title
+        </Typography>
 
-            </Toolbar>
-        </AppBar>
-    )
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 const SearchMenu = () => {
 
-    const [open, setOpen] = useState(false)
+  const [openSearchMenu, setOpenSearchMenu] = useState(false),
+    handleSearchMenuOpen = () => setOpenSearchMenu(true),
+    handleSearchMenuClose = () => setOpenSearchMenu(false)
 
-    const TopBar = () => (
+  const TopBar = () => (
 
-        <Paper component="form" sx={{ p: "2px 4px", borderRadius: 0, display: "flex", alignItems: "center", width: "full" }}>
+    <Paper
+      elevation={0}
+      component="form"
+      sx={{ p: "2px 4px", borderRadius: 0, display: "flex", alignItems: "center", width: "full" }}
+    >
+      <SideDrawer/>
+      <InputBase placeholder='Search for products' sx={{ ml: 1, flex: 1 }} />
+      <IconButton >
+        <Icon path={mdiMagnify} size={1.0} />
+      </IconButton>
+      <Divider sx={{ height: 28, m: 0.5 }} orientation='vertical' />
+      <IconButton onClick={handleSearchMenuOpen}>
+        <Icon path={mdiTune} size={1.0} />
+      </IconButton>
 
-            <IconButton sx={{ p: "10px" }}>
-                <Icon path={mdiMenu} size={1.0} />
-            </IconButton>
-            <InputBase placeholder='Search for products' sx={{ ml: 1, flex: 1 }} />
-            <IconButton >
-                <Icon path={mdiMagnify} size={1.0} />
-            </IconButton>
-            <Divider sx={{ height: 28, m: 0.5 }} orientation='vertical' />
-            <IconButton onClick={() => setOpen(!open)}>
-                <Icon path={mdiTune} size={1.0} />
-            </IconButton>
+    </Paper>
 
-        </Paper>
+  )
 
-    )
+  const SearchDrawer = () => (
+    <Drawer anchor={"bottom"} open={openSearchMenu} onClose={handleSearchMenuClose}>
+      <Box >
+        <Button>abc</Button>
+        <Button>abc</Button>
 
-    const SearchDrawer = () => (
-        <Drawer anchor={"bottom"} open={open} onClose={() => setOpen(false)}>
-            <Box>
-                <Button>abc</Button>
-            </Box>
-        </Drawer>
-    )
+      </Box>
+    </Drawer>
+  )
 
-    return (
-        <AppBar position="sticky">
-            <TopBar />
-            <SearchDrawer />
-        </AppBar>
+  return (
+    <>
+      <TopBar />
+     
+      <SearchDrawer />
+    </>
 
-    )
+  )
 }
 
 
 export default function Home() {
 
-    const list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]//, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  const list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]//, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    return (
-        <>
+  return (
+    <>
+      <div className='w-full'>
+        <AppBar position="sticky">
+          <SearchMenu />
+        </AppBar>
 
-            <div className='w-full'>
-                <AppBar position="sticky">
-                    <SearchMenu />
-                </AppBar>
-
-
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 m-4'>
-                    {list.map((value, index) => (
-                        <Item key={index} data={values.items[index % 6]} index={index} />
-                    ))}
-                </div>
-                <Pagination count={11} defaultPage={1} className="my-4 w-fit mx-auto" />
-            </div>
-        </>
-
-    )
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 m-4'>
+          {list.map((value, index) => (
+            <Item key={index} data={values.items[index % 6]} index={index} />
+          ))}
+        </div>
+        <Pagination count={11} defaultPage={1} className="my-4 w-fit mx-auto" />
+      </div>
+    </>
+  )
 }
 
 function Item({ index, data }: { index: number, data: any }) {
 
-    return (
-        <>
-            <Card >
-                <CardMedia
-                    image={`/${index % 6 + 1}.jpg`}
-                    sx={{ height: 200 }}
-                    title="skip"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {`(${data.quantity}) ${data.item}`}
-                    </Typography>
-                    <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
-                        <Icon path={mdiAccount} size={1.0} />
-                        {data.company}
-                    </Typography>
-                    <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
-                        <Icon path={mdiMapMarker} size={1.0} />
-                        {data.location}
-                    </Typography>
-                    <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
-                        <Icon path={mdiPhone} size={1.0} />
+  return (
+    <>
+      <Card >
+        <CardMedia
+          image={`/${index % 6 + 1}.jpg`}
+          sx={{ height: 200 }}
+          title="skip"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {`(${data.quantity}) ${data.item}`}
+          </Typography>
+          <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
+            <Icon path={mdiAccount} size={1.0} />
+            {data.company}
+          </Typography>
+          <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
+            <Icon path={mdiMapMarker} size={1.0} />
+            {data.location}
+          </Typography>
+          <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
+            <Icon path={mdiPhone} size={1.0} />
 
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button>
-                        More info
-                    </Button>
-                </CardActions>
-            </Card>
-        </>
-    )
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button>
+            More info
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  )
 }
