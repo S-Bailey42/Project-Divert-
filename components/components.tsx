a7509ec50308162295bf36b4f34319e4905d5c7b
evilwizard13 <evilwizard13@protonmail.com>
2024-04-15 15:59:00 +0100

    added stuff

--- components/components.tsx
+++ components/components.tsx
@@ -1,40 +1,49 @@
 "use-client"
 import Icon from "@mdi/react";
-import { mdiMenu } from "@mdi/js";
+import { mdiMenu, mdiLogout } from "@mdi/js";
 import { Box, Button, Drawer, IconButton } from "@mui/material"
 import { useState } from "react"
 
 export function SideDrawer() {
-    const toggleDrawer = (newOpen: boolean) => () => {
-        setOpen(newOpen)
-    };
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen)
+  };
 
 
 
-    const [open, setOpen] = useState(false)
-    const DrawerList = (
-        <div>
-            <IconButton
-                onClick={toggleDrawer(true)}
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-            >
-                <Icon size={1.0} path={mdiMenu} />
-            </IconButton>
-            <Drawer open={open} onClose={toggleDrawer(false)}>
-                <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+  const [open, setOpen] = useState(false)
 
-                </Box>
-            </Drawer>
-        </div>
-    )
+  const OpenButton = () => (
+    <IconButton
+      onClick={toggleDrawer(true)}
+      size="large"
+      edge="start"
+      aria-label="menu"
+      sx={{ m: 0 }}
+    >
+      <Icon size={1.0} path={mdiMenu} />
+    </IconButton>
+  )
 
-    return (
-        <>
-            {DrawerList}
-        </>
-    )
+  const DrawerList = () => (
+
+
+    <Drawer open={open} onClose={toggleDrawer(false)}>
+      <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+        <Button sx={{ mt: "auto", mb: 0 }} startIcon={<Icon path={mdiLogout} size={1} />} color="error">
+          Log out
+        </Button>
+        <div className="mb-0 mt-auto flex w-10 h-10 debug-border" ></div>
+
+      </Box>
+    </Drawer>
+
+  )
+
+  return (
+    <>
+      <OpenButton />
+      <DrawerList />
+    </>
+  )
 }
