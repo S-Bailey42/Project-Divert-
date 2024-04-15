f69fa4696b4c0fdb98c21b26e67a215d1a89ec5e
evilwizard13 <evilwizard13@protonmail.com>
2024-04-15 11:29:02 +0100

    search menu

--- app/charity/page.tsx
+++ app/charity/page.tsx
@@ -7,35 +7,90 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
 import Icon from "@mdi/react";
-import { mdiAccount, mdiEmail, mdiMapMarker, mdiMenu, mdiPhone } from '@mdi/js';
+import { mdiAccount, mdiChevronDown, mdiChevronUp, mdiEmail, mdiMagnify, mdiMapMarker, mdiMenu, mdiPhone, mdiTune } from '@mdi/js';
 import { useEffect } from 'react';
-import { AppBar, Box, IconButton, Pagination, Toolbar } from '@mui/material';
+import { AppBar, Box, Divider, Drawer, IconButton, InputBase, Pagination, Paper, Stack, TextField, Toolbar } from '@mui/material';
 import { SideDrawer } from '@/components/components';
+import { useState } from 'react';
 import values from "../../values.json"
+
+const TopBar = () => {
+    return (
+        <AppBar position="sticky">
+            <Toolbar sx={{ width: "100%", maxWidth: 600, mx: "auto" }}>
+                <SideDrawer />
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Title
+                </Typography>
+
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+const SearchMenu = () => {
+
+    const [open, setOpen] = useState(false)
+
+    const TopBar = () => (
+
+        <Paper component="form" sx={{ p: "2px 4px", borderRadius: 0, display: "flex", alignItems: "center", width: "full" }}>
+
+            <IconButton sx={{ p: "10px" }}>
+                <Icon path={mdiMenu} size={1.0} />
+            </IconButton>
+            <InputBase placeholder='Search for products' sx={{ ml: 1, flex: 1 }} />
+            <IconButton >
+                <Icon path={mdiMagnify} size={1.0} />
+            </IconButton>
+            <Divider sx={{ height: 28, m: 0.5 }} orientation='vertical' />
+            <IconButton onClick={() => setOpen(!open)}>
+                <Icon path={mdiTune} size={1.0} />
+            </IconButton>
+
+        </Paper>
+
+    )
+
+    const SearchDrawer = () => (
+        <Drawer anchor={"bottom"} open={open} onClose={() => setOpen(false)}>
+            <Box>
+                <Button>abc</Button>
+            </Box>
+        </Drawer>
+    )
+
+    return (
+        <AppBar position="sticky">
+            <TopBar />
+            <SearchDrawer />
+        </AppBar>
+
+    )
+}
+
 
 export default function Home() {
 
     const list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]//, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     return (
+        <>
 
-        <div className='w-full'>
-            <AppBar position="sticky">
-                <Toolbar sx={{ width: "100%", maxWidth: 600, mx: "auto" }}>
-                    <SideDrawer />
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Title
-                    </Typography>
+            <div className='w-full'>
+                <AppBar position="sticky">
+                    <SearchMenu />
+                </AppBar>
 
-                </Toolbar>
-            </AppBar>
-            <div className='flex grid-cols-1 lg:grid-cols-3 grid gap-4 m-4'>
-                {list.map((value, index) => (
-                    <Item key={index} data={values.items[index % 6]} index={index} />
-                ))}
+
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 m-4'>
+                    {list.map((value, index) => (
+                        <Item key={index} data={values.items[index % 6]} index={index} />
+                    ))}
+                </div>
+                <Pagination count={11} defaultPage={1} className="my-4 w-fit mx-auto" />
             </div>
-            <Pagination count={11} defaultPage={1} className="my-4 w-fit mx-auto" />
-        </div>
+        </>
 
     )
 }
