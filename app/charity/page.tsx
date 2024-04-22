e94395e6b05a6bc60252a723a60dc1c9a0687c35
evilwizard13 <evilwizard13@protonmail.com>
2024-04-22 15:56:05 +0100

    list and grid

--- app/charity/page.tsx
+++ app/charity/page.tsx
@@ -28,7 +28,7 @@ const TopBar = () => {
   )
 }
 
-const SearchMenu = () => {
+const SearchMenuMobile = () => {
 
   const [openSearchMenu, setOpenSearchMenu] = useState(false),
     handleSearchMenuOpen = () => setOpenSearchMenu(true),
@@ -41,7 +41,7 @@ const SearchMenu = () => {
       component="form"
       sx={{ p: "2px 4px", borderRadius: 0, display: "flex", alignItems: "center", width: "full" }}
     >
-      <SideDrawer/>
+      <SideDrawer />
       <InputBase placeholder='Search for products' sx={{ ml: 1, flex: 1 }} />
       <IconButton >
         <Icon path={mdiMagnify} size={1.0} />
@@ -68,48 +68,64 @@ const SearchMenu = () => {
   return (
     <>
       <TopBar />
-     
+
       <SearchDrawer />
     </>
 
   )
 }
 
+const ItemArea = ({ itemView, items }: { itemView: string, items: any }) => {
+  if (itemView == "list") {
+    return (
+      <div className=" grid flex flex-col gap-4 flex-grow  m-4">
+        {items.map((value, index) => (
+          <ListItem key={index} data={values.items[index % 6]} index={index} />
+        ))}
+      </div>
+    )
+  }
+}
+
 
 export default function Home() {
 
+
+
   const list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]//, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  const [itemView, setItemView] = useState("list")
 
   return (
     <>
       <div className='w-full'>
-        <AppBar position="sticky">
-          <SearchMenu />
+        <AppBar position="sticky" className="mt-4">
+          <SearchMenuMobile />
         </AppBar>
 
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 m-4'>
-          {list.map((value, index) => (
-            <Item key={index} data={values.items[index % 6]} index={index} />
-          ))}
-        </div>
+        <ItemArea itemView={itemView} items={list} />
+
         <Pagination count={11} defaultPage={1} className="my-4 w-fit mx-auto" />
       </div>
     </>
   )
 }
 
-function Item({ index, data }: { index: number, data: any }) {
-
+function ListItem({ index, data }: { index: number, data: any }) {
   return (
-    <>
-      <Card >
-        <CardMedia
-          image={`/${index % 6 + 1}.jpg`}
-          sx={{ height: 200 }}
-          title="skip"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+
+    <Card sx={{ display: 'flex', width: "full" }}>
+      <CardMedia
+        component="img"
+        image={`/${index % 6 + 1}.jpg`}
+        sx={{ height: 250, width: 350 }}
+        title="skip"
+      />
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+
+
+        <CardContent className=" my-auto debug-border w-fit">
+          <Typography sx={{ flex: "none" }} gutterBottom variant="h5"  >
             {`(${data.quantity}) ${data.item}`}
           </Typography>
           <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
@@ -122,15 +138,56 @@ function Item({ index, data }: { index: number, data: any }) {
           </Typography>
           <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
             <Icon path={mdiPhone} size={1.0} />
-
+            {data.phoneNumber}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button>
-            More info
-          </Button>
-        </CardActions>
-      </Card>
-    </>
+
+
+      </Box>
+      <CardActions className='debug-border grid ml-auto mr-0   gap-2'>
+        <Button variant="outlined" size="large">
+          More info
+        </Button>
+
+
+
+      </CardActions>
+    </Card>
+
+  )
+}
+
+function GridItem({ index, data }: { index: number, data: any }) {
+
+  return (
+    <Card >
+      <CardMedia
+        image={`/${index % 6 + 1}.jpg`}
+        sx={{ height: 200 }}
+        title="skip"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {`(${data.quantity}) ${data.item}`}
+        </Typography>
+        <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
+          <Icon path={mdiAccount} size={1.0} />
+          {data.company}
+        </Typography>
+        <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
+          <Icon path={mdiMapMarker} size={1.0} />
+          {data.location}
+        </Typography>
+        <Typography variant="body1" color="text.secondary" className="flex flex-row gap-2">
+          <Icon path={mdiPhone} size={1.0} />
+
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button>
+          More info
+        </Button>
+      </CardActions>
+    </Card>
   )
 }
