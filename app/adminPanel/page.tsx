66cf1fc849f142e8127c3b14b99d2caf3ad650d9
S-Bailey42 <SamuelPWB@hotmail.com>
2024-04-22 15:48:48 +0100

    Final update 22/04/24

--- app/adminPanel/page.tsx
+++ app/adminPanel/page.tsx
@@ -237,7 +237,7 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
           id="tableTitle"
           component="div"
         >
-          Nutrition
+          Requested Accounts
         </Typography>
       )}
       {numSelected > 0 ? (
