db6af38c3fe22111fb2e8647dc51d4af982431bc
S-Bailey42 <SamuelPWB@hotmail.com>
2024-04-29 15:51:18 +0100

    Changes on 29/04/24

--- app/adminPanel/page.tsx
+++ app/adminPanel/page.tsx
@@ -1,67 +1,181 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import FolderIcon from "@mui/icons-material/Folder";
+import { SideDrawer } from "@/components/components";
+import { useState } from "react";
+import { FitScreen } from "@mui/icons-material";
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'User ID', width: 70 },
-  { field: 'companyName', headerName: 'Company Name', width: 130 },
-  { field: 'email', headerName: 'Email', width: 200 },
+  { field: "id", headerName: "User ID", width: 200 },
+  { field: "companyName", headerName: "Company Name", width: 400 },
+  { field: "email", headerName: "Email", width: 300 },
   {
-    field: 'accountType',
-    headerName: 'Account Type',
-    type: 'number',
+    field: "accountType",
+    headerName: "Account Type",
+    type: "number",
     width: 100,
   },
   {
-    field: 'postCode',
-    headerName: 'Post Code',
-    width: 100
+    field: "postCode",
+    headerName: "Post Code",
+    width: 100,
   },
 ];
 
-const rows = [
-  { id: 1, email: 'deyor96485@haislot.com', companyName: 'Morgan Sindall', accountType: 2, postCode: 'NN6 8HJ' },
-  { id: 2, email: 'gaiaferran@pastipass.com', companyName: 'Accomodation Concern', accountType: 1, postCode: 'OX5 2FG' },
-  { id: 3, email: 'opera315@alimoh.cloud', companyName: 'Return MK', accountType: 1, postCode: 'SH15 8JJ' },
-  { id: 4, email: 'cremnyov@rutory.com', companyName: 'The Wipers Foundation', accountType: 1, postCode: 'G6 7HU' },
-  { id: 5, email: 'mcmick@btcmod.com', companyName: 'Spencer Contact', accountType: 1, postCode: 'MK8 EDU' },
-  { id: 6, email: 'tkkolian3@loranerobinson.info', companyName: 'Overbury', accountType: 2, postCode: 'LL60 UNZ' },
-  { id: 7, email: 'elhechicero@naverly.com', companyName: 'Modus', accountType: 2, postCode: 'BG02 MWM' },
-  { id: 8, email: 'dakelenafranc@cjutro.de', companyName: 'Graeae', accountType: 1, postCode: 'KM05 AUL' },
-  { id: 9, email: 'pr1geetoe20@lushily.top', companyName: 'Emmaus', accountType: 1, postCode: 'HY59 MXB' },
-  { id: 10, email: 'bexrider@chantellegribbon.com', companyName: 'Cast Interiors', accountType: 2, postCode: 'KA54 PKR' },
-  { id: 11, email: 'madugala@clonevnmail.com', companyName: 'The Daylight Centre', accountType: 1, postCode: 'PF04 LZE' },
-  { id: 12, email: 'lilyjoe@audrianaputri.com', companyName: 'Todd', accountType: 1, postCode: 'TW4 T00' },
-];
+const Page = () => {
+  const [rows, setRows] = useState([
+    {
+      id: 1,
+      email: "deyor96485@haislot.com",
+      companyName: "Morgan Sindall",
+      accountType: 2,
+      postCode: "NN6 8HJ",
+    },
+    {
+      id: 2,
+      email: "gaiaferran@pastipass.com",
+      companyName: "Accomodation Concern",
+      accountType: 1,
+      postCode: "OX5 2FG",
+    },
+    {
+      id: 3,
+      email: "opera315@alimoh.cloud",
+      companyName: "Return MK",
+      accountType: 1,
+      postCode: "SH15 8JJ",
+    },
+    {
+      id: 4,
+      email: "cremnyov@rutory.com",
+      companyName: "The Wipers Foundation",
+      accountType: 1,
+      postCode: "G6 7HU",
+    },
+    {
+      id: 5,
+      email: "mcmick@btcmod.com",
+      companyName: "Spencer Contact",
+      accountType: 1,
+      postCode: "MK8 EDU",
+    },
+    {
+      id: 6,
+      email: "tkkolian3@loranerobinson.info",
+      companyName: "Overbury",
+      accountType: 2,
+      postCode: "LL60 UNZ",
+    },
+    {
+      id: 7,
+      email: "elhechicero@naverly.com",
+      companyName: "Modus",
+      accountType: 2,
+      postCode: "BG02 MWM",
+    },
+    {
+      id: 8,
+      email: "dakelenafranc@cjutro.de",
+      companyName: "Graeae",
+      accountType: 1,
+      postCode: "KM05 AUL",
+    },
+    {
+      id: 9,
+      email: "pr1geetoe20@lushily.top",
+      companyName: "Emmaus",
+      accountType: 1,
+      postCode: "HY59 MXB",
+    },
+    {
+      id: 10,
+      email: "bexrider@chantellegribbon.com",
+      companyName: "Cast Interiors",
+      accountType: 2,
+      postCode: "KA54 PKR",
+    },
+    {
+      id: 11,
+      email: "madugala@clonevnmail.com",
+      companyName: "The Daylight Centre",
+      accountType: 1,
+      postCode: "PF04 LZE",
+    },
+    {
+      id: 12,
+      email: "lilyjoe@audrianaputri.com",
+      companyName: "Todd",
+      accountType: 1,
+      postCode: "TW4 T00",
+    },
+  ]);
 
-const page = () => {
+  const [selectedRows, setSelectedRows] = useState<Array<number>>([]);
+
+  function handleClick() {
+    setRows(rows.filter((value) => !selectedRows.includes(value.id)));
+  }
+
   return (
-    <div id='parent'>
-      <div style={{ height: 580, width: '100%' }}>
-        <div className='pb-3 pt-3 text-center underline' id='title'>Accounts Requesting Access</div>
+    <div id="parent">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <SideDrawer />
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Accounts Requesting Access
+            </Typography>
+            <Button
+              variant="contained"
+              color="success"
+              className=" absolute right-10"
+              onClick={handleClick}
+            >
+              Accept
+            </Button>
+            <Button variant="contained" color="error">
+              Reject
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box sx={{ height: "FitScreen", width: "100%", minHeight: "100%" }}>
         <DataGrid
+          style={{
+            border: "solid",
+            minWidth: "100%",
+            height: "95vh",
+          }}
+          sx={{ flex: 1 }}
           rows={rows}
           columns={columns}
+          autoHeight={true}
           initialState={{
             pagination: {
-              paginationModel: { page: 0, pageSize: 10 },
+              paginationModel: { page: 0, pageSize: 20 },
             },
           }}
-          pageSizeOptions={[10, 20]}
+          pageSizeOptions={[20, 40]}
           checkboxSelection
+          onRowSelectionModelChange={(e) => setSelectedRows(e)}
         />
-
-      </div>
-      <div id='buttonAccept' className='absolute w-16 h-8 left-0 phone:left-28'>
-        <Button color="success">Accept</Button>
-      </div>
-      <div id='buttonAccept' className='absolute w-16 h-8 left-16 phone:left-48'>
-        <Button color="error">Reject</Button>
-      </div>
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
--- components/components.tsx
+++ components/components.tsx
@@ -1,49 +1,47 @@
-"use-client"
+"use-client";
 import Icon from "@mdi/react";
 import { mdiMenu, mdiLogout } from "@mdi/js";
-import { Box, Button, Drawer, IconButton } from "@mui/material"
-import { useState } from "react"
+import { Box, Button, Drawer, IconButton } from "@mui/material";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export function SideDrawer() {
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen)
-  };
-
-
-
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false),
+    handleDrawerOpen = () => setOpen(true),
+    handleDrawerClose = () => setOpen(false);
 
   const OpenButton = () => (
     <IconButton
-      onClick={toggleDrawer(true)}
+      onClick={handleDrawerOpen}
       size="large"
       edge="start"
+      color="inherit"
       aria-label="menu"
-      sx={{ m: 0 }}
+      sx={{ mr: 2 }}
     >
-      <Icon size={1.0} path={mdiMenu} />
+      <MenuIcon />
     </IconButton>
-  )
+  );
 
   const DrawerList = () => (
-
-
-    <Drawer open={open} onClose={toggleDrawer(false)}>
-      <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-        <Button sx={{ mt: "auto", mb: 0 }} startIcon={<Icon path={mdiLogout} size={1} />} color="error">
+    <Drawer open={open} onClose={handleDrawerClose}>
+      <Box sx={{ width: 250 }} role="presentation" onClick={handleDrawerClose}>
+        <Button
+          sx={{ mt: "auto", mb: 0 }}
+          startIcon={<Icon path={mdiLogout} size={1} />}
+          color="error"
+        >
           Log out
         </Button>
-        <div className="mb-0 mt-auto flex w-10 h-10 debug-border" ></div>
-
+        <div className="mb-0 mt-auto flex w-10 h-10 debug-border"></div>
       </Box>
     </Drawer>
-
-  )
+  );
 
   return (
     <>
       <OpenButton />
       <DrawerList />
     </>
-  )
+  );
 }
