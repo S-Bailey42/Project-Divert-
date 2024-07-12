c63258ffa4573b939cc065d7a5d3c4c2da2840b8
S-Bailey42 <143730217+S-Bailey42@users.noreply.github.com>
2024-07-12 11:40:35 +0100

    Moving the home page and updating the get site ID function

--- app/utils/token.tsx
+++ app/utils/token.tsx
@@ -10,11 +10,16 @@ export const getToken: any = (token: any) => {
   return localStorage.getItem("authToken")
 };
 
-//create code below that uses Regex to extract user ID from auth Token
-
 export const getUserId: any = (token: any) => {
   const newToken = getToken();
   const parsedToken = JSON.parse(newToken);
   const userId = parsedToken.user_id;
   return userId;
 };
+
+export const getSiteId: any = (token: any) => {
+  const newToken = getToken();
+  const parsedToken = JSON.parse(newToken);
+  const siteId = parsedToken.site_id;
+  return siteId;
+}
